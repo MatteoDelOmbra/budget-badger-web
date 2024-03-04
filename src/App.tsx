c23bd1927e8b6c8b5544d78dc98ebafb585e9952
mateusz.wojtania <mateusz99w@gmail.com>
2024-03-04 23:19:46 +0100

    add favicon adding styled components base look of main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <p>BudgetBadger</p>;
-}
+import { Add } from "./components/Add";
+import { MainContainer } from "./components/MainContainer";
+import { NavBar } from "./components/NavBar";
+import { PageWrapper } from "./components/PageWrapper";
 
+function App() {
+  return (
+    <>
+      <PageWrapper>
+        <NavBar>navbar here</NavBar>
+        <MainContainer>Main</MainContainer>
+        <Add>Add</Add>
+      </PageWrapper>
+    </>
+  );
+}
 export default App;
---
+++ src/components/Add.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Add = styled.button`
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+`;
