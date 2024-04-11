910b23f8473bdc09b25876423788ea1bb6c4e1a5
mateusz.wojtania <mateusz.wojtania@frends.com>
2024-04-11 14:10:39 +0200

    remove white strap while scrolling

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,12 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
-body {
+*{
   margin: 0;
   padding: 0;
-}
-
-*{
   font-family: Thasadith;
   color: ${(props) => props.theme.primaryText};
+  background-color: ${(props) => props.theme.primary};
 }
 `;
